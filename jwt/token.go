c58ec003beaa807818c9a54c4e2bebd33b9801ac
Fredrik Söderström <fredrik.soderstrom@mojlighetsministeriet.se>
2017-09-22 00:04:40 +0200

    Move jwt stuff to utils

---
+++ jwt/token.go
@@ -0,0 +1,48 @@
+package jwt
+
+import (
+	"crypto/rsa"
+	"time"
+
+	"github.com/mojlighetsministeriet/identity-provider/entity"
+
+	"github.com/SermoDigital/jose/crypto"
+	"github.com/SermoDigital/jose/jws"
+	josejwt "github.com/SermoDigital/jose/jwt"
+)
+
+// Generate a new JWT token from a user
+func Generate(issuer string, privateKey *rsa.PrivateKey, account entity.Account) (serializedToken []byte, err error) {
+	claims := jws.Claims{}
+
+	account.BeforeSave()
+
+	claims.SetExpiration(time.Now().Add(time.Duration(60*20) * time.Second))
+	claims.SetSubject(account.ID)
+	claims.SetIssuer(issuer)
+	claims.Set("email", account.Email)
+	claims.Set("roles", account.RolesSerialized)
+
+	token := jws.NewJWT(claims, crypto.SigningMethodRS256)
+
+	serializedToken, err = token.Serialize(privateKey)
+
+	return
+}
+
+// ParseIfValid return a parsed JWT token if it is valid
+func ParseIfValid(publicKey *rsa.PublicKey, token []byte) (token josejwt.JWT, err error) {
+	token, err = jws.ParseJWT(token)
+	if err != nil {
+		return
+	}
+
+	err = token.Validate(publicKey, crypto.SigningMethodRS256)
+	if err != nil {
+		claims := jws.Claims{}
+		claims.SetExpiration(time.Now().Add(time.Duration(60*20) * time.Second))
+		token = jws.NewJWT(claims, crypto.SigningMethodRS256)
+	}
+
+	return
+}
---
+++ jwt/token_test.go
@@ -0,0 +1,62 @@
+package jwt_test
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"testing"
+
+	"github.com/mojlighetsministeriet/identity-provider/entity"
+	"github.com/mojlighetsministeriet/utils/jwt"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateAndParseIfValid(test *testing.T) {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 1024)
+	assert.NoError(test, err)
+
+	account := entity.Account{
+		ID:    uuid.NewV4().String(),
+		Email: "tech+testing@mojlighetsministerietest.se",
+		Roles: []string{"user"},
+	}
+
+	accessToken, err := jwt.Generate("test-service", privateKey, account)
+	assert.NoError(test, err)
+
+	parsedToken, err := jwt.ParseIfValid(&privateKey.PublicKey, accessToken)
+	assert.NoError(test, err)
+	assert.Equal(test, account.ID, parsedToken.Claims().Get("sub").(string))
+	assert.Equal(test, "tech+testing@mojlighetsministerietest.se", parsedToken.Claims().Get("email"))
+	assert.Equal(test, "user", parsedToken.Claims().Get("roles"))
+}
+
+func TestFailParseIfValidWithBadToken(test *testing.T) {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 1024)
+	assert.NoError(test, err)
+
+	parsedToken, err := jwt.ParseIfValid(&privateKey.PublicKey, []byte{1, 2, 3, 4})
+	assert.Error(test, err)
+	assert.Equal(test, nil, parsedToken)
+}
+
+func TestFailParseIfValidWithBadPublicKey(test *testing.T) {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 1024)
+	assert.NoError(test, err)
+
+	account := entity.Account{
+		ID:    uuid.NewV4().String(),
+		Email: "tech+testing@mojlighetsministerietest.se",
+		Roles: []string{"user"},
+	}
+
+	accessToken, err := jwt.Generate("test-service", privateKey, account)
+	assert.NoError(test, err)
+
+	wrongPrivateKey, err := rsa.GenerateKey(rand.Reader, 1024)
+	assert.NoError(test, err)
+
+	parsedToken, err := jwt.ParseIfValid(&wrongPrivateKey.PublicKey, accessToken)
+	assert.Error(test, err)
+	assert.Equal(test, false, parsedToken.Claims().Has("email"))
+}
